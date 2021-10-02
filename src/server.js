c42e342a8a704dfa2ad939c77597d50ca4bf2152
Derek G Chen <derek@derekgc.com>
2021-10-02 11:58:51 -0400

    update server

--- src/server.js
+++ src/server.js
@@ -29,6 +29,20 @@ client.keys('*', function (err, keys) {
   }
 }); 
 
+const hsetFunction = (key,field, value) =>{
+  client.hset(key,field, value, (err, reply) =>{
+    if (err) throw err;
+    console.log(reply);
+  
+  })
+}
+
+const exampleData = {test1:{je:'hello', be:'holla'}, test2:{wfw:'wfw', field2:'something'}};
+
+hsetFunction('mysimplehash', 'first', 'wfwfee');
+hsetFunction('mysimplehash', 'second', 'wfwfee second');
+hsetFunction('mysimplehash', 'whattt', 'wfwfee whattt');
+
 client.hgetall('*', function(err, keys){
   console.log("hgetall");
   if (err) return console.log(err);
